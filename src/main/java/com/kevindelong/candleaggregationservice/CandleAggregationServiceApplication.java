3836606e098b271e4185f145704ae782074ac091
kevinde35 <kevindelong425@outlook.com>
2026-02-28 01:54:06 +0500

    chore: bootstrap spring boot project structure

---
+++ src/main/java/com/kevindelong/candleaggregationservice/CandleAggregationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kevindelong.candleaggregationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandleAggregationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CandleAggregationServiceApplication.class, args);
+    }
+
+}
